cd1da90565a63c9bbe55d6f7c313967da3a8ea79
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-01-15 09:46:30 +0530

    Added solution - LeetHub

---
+++ 3. Quadratic Equation Roots - GFG/3.-quadratic-equation-roots.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> quadraticRoots(int a, int b, int c) {
+        // code here
+        vector<int>v;
+        double D=sqrt(b*b-4*a*c);
+        if(D>=0){
+            double d=(floor((-b+D)/(2.0*a)));
+            double e=(floor((-b-D)/(2.0*a)));
+            if(d>=e){
+                v.push_back(d);
+                v.push_back(e);
+            }else{
+                v.push_back(e);
+                v.push_back(d);
+            }
+        }
+        else v.push_back(-1);
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        Solution ob;
+        vector<int> roots = ob.quadraticRoots(a, b, c);
+        if (roots.size() == 1 && roots[0] == -1)
+            cout << "Imaginary";
+        else
+            for (int i = 0; i < roots.size(); i++) cout << roots[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
