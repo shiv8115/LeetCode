f6c23e01e7dc8f96c73e7c8dece78eeee8386875
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-01-17 12:25:33 +0530

    Added solution - LeetHub

--- Minimum swaps and K together - GFG/minimum-swaps-and-k-together.cpp
+++ Minimum swaps and K together - GFG/minimum-swaps-and-k-together.cpp
@@ -15,19 +15,25 @@ class Solution
 public:
     int minSwap(int arr[], int n, int k) {
         // Complet the function
-        int good=0,bad=0;
+        int window=0,bad=0;
         for(int i=0;i<n;i++){
-            if(arr[i]<=k) good++;
+            if(arr[i]<=k) window++;
         }
-        for(int i=0;i<good;i++){
+        for(int i=0;i<window;i++){
             if(arr[i]>k) bad++;
         }
         int ans=bad;
-        for(int i=0,j=good;j<n;i++,j++){
-            if(arr[i]>k) --bad;
-            if(arr[j]>k) ++bad;
-            ans=min(ans,bad);
-        }
+         for(int i=window;i<n;i++)
+       {
+           if(arr[i-window]>k)
+           bad--;
+           
+           if(arr[i]>k)
+           bad++;
+           
+           
+           ans=min(ans,bad);
+       }
         return ans;
     }
 };
