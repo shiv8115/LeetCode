3abbbd1c72061ff5ec0de4a1997eac918fff1ba2
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-01-20 17:20:38 +0530

    Time: 2 ms (31.42%), Space: 6.2 MB (62.97%) - LeetHub

---
+++ 796-rotate-string/796-rotate-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string str1, string str2) {
+        if (str1.length() != str2.length())
+        return false;
+ 
+   string temp = str1 + str1;
+  return (temp.find(str2) != string::npos);
+        
+    }
+};
