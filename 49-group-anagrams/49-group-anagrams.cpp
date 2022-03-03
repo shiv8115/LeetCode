c5065f2927b038fc8b86d00ccd628c9118d87d43
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-03-03 15:14:48 +0530

    Time: 32 ms (91.57%), Space: 19.5 MB (81.17%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,13 +3,13 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         // vector<vector<string>>anagrams;
         unordered_map<string, vector<string>> mp;
-        for (string s : strs) {
+        for (string &s : strs) {
             string t = s; 
             sort(t.begin(), t.end());
             mp[t].push_back(s);
         }
         vector<vector<string>> anagrams;
-        for (auto p : mp) { 
+        for (auto &p : mp) { 
             anagrams.push_back(p.second);
         }
         return anagrams;
