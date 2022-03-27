6f6578385eee35bcd93577a1203e5b9e96ee0487
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-03-27 12:04:29 +0530

    Time: 8 ms (95.46%), Space: 7.6 MB (70.07%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    // int lcs(string s1, string s2){
-    //     int n= s1.length(), m= s2.length();
-    //     int dp[n+1][m+1];
-    //     for(int i=0;i<=n; i++){
-    //         for(int j=0;j<=m;j++){
-    //             if(i==0) dp[i][j]=0;
-    //             else if(j==0) dp[i][j]=0;
-    //             else{
-    //                 if(s1[i-1]==s2[j-1]) 
-    //                     dp[i][j]= dp[i-1][j-1]+1;
-    //                 else dp[i][j]= max(dp[i+1][j], dp[i][j+1]);
-    //             }
-    //         }
-    //     }
-    //     return dp[n][m];
-    // }
-    int lcs(int x, int y, string s1, string s2)
-    {
-        // your code here
-        int dp[x+1][y+1];
-        for(int i=0;i<=x;i++) dp[i][0]=0;
-        for(int j=0;j<=y;j++) dp[0][j]=0;
-        for(int i=1;i<=x;i++){
-            for(int j=1;j<=y;j++){
-                if(s1[i-1]==s2[j-1])
-                dp[i][j]=1+dp[i-1][j-1];
-                else
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+    int lcs(int n, int m, string s1, string s2){
+        int dp[n+1][m+1];
+        for(int i=0;i<=n; i++){
+            for(int j=0;j<=m;j++){
+                if(i==0) dp[i][j]=0;
+                else if(j==0) dp[i][j]=0;
+                else{
+                    if(s1[i-1]==s2[j-1]) 
+                        dp[i][j]= dp[i-1][j-1]+1;
+                    else dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-        return dp[x][y];
+        return dp[n][m];
     }
+    // int lcs(int x, int y, string s1, string s2)
+    // {
+    //     // your code here
+    //     int dp[x+1][y+1];
+    //     for(int i=0;i<=x;i++) dp[i][0]=0;
+    //     for(int j=0;j<=y;j++) dp[0][j]=0;
+    //     for(int i=1;i<=x;i++){
+    //         for(int j=1;j<=y;j++){
+    //             if(s1[i-1]==s2[j-1])
+    //             dp[i][j]=1+dp[i-1][j-1];
+    //             else
+    //             dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[x][y];
+    // }
     int minDistance(string word1, string word2) {
         int n1= word1.size();
         int n2= word2.size();
