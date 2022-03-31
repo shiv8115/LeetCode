85de97db6ad2541960ffb64a975ee3cb74863df8
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-03-31 23:49:43 +0530

    Time: 0 ms (100.00%), Space: 7 MB (99.94%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,64 +1,33 @@
 class Solution {
 public:
-    // bool isValid(vector<int>&arr, int n, int k){
-    //     int sum=0;
-    //     int count=1;
-    //     for(int i=0;i<n;i++){
-    //         sum+= arr[i];
-    //         if(sum>k) {
-    //             count++;
-    //             sum=arr[i];
-    //         }
-    //     }
-    //     return (count<=k);
-    // }
-    // int splitArray(vector<int>& nums, int m) {
-    //     int ans=-1;
-    //     int n= nums.size();
-    //     int low=0, high=0;
-    //     for(auto &x: nums){
-    //         low= max(low, x);
-    //         high+= x;
-    //     }
-    //     while(low<=high){
-    //         int mid= low+(high-low)/2;
-    //         if(isValid(nums,n,mid)) {
-    //             ans= mid;
-    //             high=mid-1;
-    //         }else low= mid+1;
-    //     }
-    //       return ans;
-    // }
-    bool is_possible_sum(vector<int>&v, int mid, int days){
-        int d=1;
+    bool isValid(vector<int>&arr, int k, int m){
         int sum=0;
-        for(int i=0;i<v.size();i++){
-            sum+=v[i];
-            if(sum>mid){
-                d++;
-                sum=v[i];
+        int count=1;
+        for(int i=0;i<arr.size();i++){
+            sum+= arr[i];
+            if(sum>k) {
+                count++;
+                sum=arr[i];
             }
         }
-        return d <= days;
+        return (count<=m);
     }
-    int splitArray(vector<int>& weights, int days) {
+    int splitArray(vector<int>& nums, int m) {
         int ans=0;
-        int n=weights.size();
-        int maxm=0,sum=0;
-        for(auto val : weights){
-            sum+=val;
-            maxm=max(maxm,val);
+        int n= nums.size();
+        int low=0, high=0;
+        for(auto &x: nums){
+            low= max(low, x);
+            high+= x;
         }
-        if(days==n) return maxm;
-        int low=maxm, high=sum;
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(is_possible_sum(weights,mid,days) == true){
+            int mid= low+(high-low)/2;
+            if(isValid(nums,mid,m)) {
                 ans= mid;
                 high=mid-1;
-            }else low=mid+1;
+            }else low= mid+1;
         }
-        return ans;
+          return ans;
         
     }
 };
