3a74aea9a14af1b8dabb14894442224cdfcb3cd9
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-04-19 22:24:50 +0530

    Time: 26 ms (91.81%), Space: 58.2 MB (23.92%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+ void inor(TreeNode*&root,vector<TreeNode*>&v){
+        if(root==nullptr)return;
+        inor(root->left,v);
+        v.push_back(root);
+        inor(root->right,v);
+    }
+    void recoverTree(TreeNode* root) {
+        vector<TreeNode*>inorder;
+        inor(root,inorder);
+        int first=-1,second=-1,third=-1;
+        for(int i=0;i<inorder.size()-1;i++){
+            if(inorder[i]->val>inorder[i+1]->val){
+                if(first==-1&&second==-1){
+                first=i;
+                second=i+1;
+                }
+                else{third=i+1;}
+            }
+        }
+            if(third==-1){
+                int temp=inorder[first]->val;
+                inorder[first]->val=inorder[second]->val;
+                inorder[second]->val=temp;
+            }
+            else{
+                 int temp=inorder[first]->val;
+                inorder[first]->val=inorder[third]->val;
+                inorder[third]->val=temp;
+                }
+             }
+};
