48ca97775939b1461f2468746a3972572dd94c10
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-05-24 19:39:48 +0530

    Time: 4 ms (73.76%), Space: 7.4 MB (42.55%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -3,14 +3,14 @@ public:
     int longestValidParentheses(string s) {
         int res=0;
         stack<int>st;
-        st.push(-1);
+        st.push(-1); // Index Stack
         for(int i=0;i<s.size();i++){
             if(s[i]=='(') st.push(i);
             else{
-                st.pop();
-                if(st.empty()) st.push(i);
+                st.pop(); // If we get closing bracket
+                if(st.empty()) st.push(i); // If stack is empty
                 else{
-                    res= max(res, i-st.top());
+                    res= max(res, i-st.top()); // If not empty then update answer.
                 }
             }
         }
