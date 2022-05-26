72e85fd13e27f6565ff84e15fc71a9043f12b41c
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-05-27 00:42:43 +0530

    Added solution - LeetHub

--- Length of the longest substring - GFG/length-of-the-longest-substring.cpp
+++ Length of the longest substring - GFG/length-of-the-longest-substring.cpp
@@ -6,49 +6,50 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    int longestUniqueSubsttr(string S){
+    int longestUniqueSubsttr(string s){
         //code
-    //     int n=s.length(), i=-1, j=-1, ans=0;
-    //     unordered_map<char,int>mp;
-    //     while(true){
-    //         // Aquire
-    //         bool f1= false, f2= false;
-    //         while(i<n-1){
-    //             f1=true;
-    //             i++;
-    //             mp[s[i]]++;
-    //             if(mp[s[i]]==2) break;
-    //             else{
-    //                 int len= i-j;
-    //                 if(len>ans) ans=len;
-    //             }
-    //         }
-            
-    //         // Release
-    //         while(j<i){
-    //             f2=true;
-    //             j++;
-    //             mp[s[j]]--;
-    //             if(mp[s[j]]==1) break;
-    //         }
-    //         if(f1==false && f2== false) break;
-    //     }
-    //   return ans; 
-    int n=S.length(),i=0,j=0,mx=INT_MIN;
-        int m[26]={0};
-        
-        while(j<n){
-            m[S[j]-'a']++;
-            if(m[S[j]-'a']>1){
-                while(m[S[j]-'a']>1){
-                    m[S[i]-'a']--;
-                    i++;
+        int n=s.length(), i=-1, j=-1, ans=0;
+       // unordered_map<char,int>mp;
+        int mp[26]={0};
+        while(true){
+            // Aquire
+            bool f1= false, f2= false;
+            while(i<n-1){
+                f1=true;
+                i++;
+                mp[s[i]-'a']++;
+                if(mp[s[i]-'a']==2) break;
+                else{
+                    int len= i-j;
+                    if(len>ans) ans=len;
                 }
             }
-            mx=max(mx,j-i+1);
-            j++;
+            
+            // Release
+            while(j<i){
+                f2=true;
+                j++;
+                mp[s[j]-'a']--;
+                if(mp[s[j]-'a']==1) break;
+            }
+            if(f1==false && f2== false) break;
         }
-        return mx;
+      return ans; 
+    // int n=S.length(),i=0,j=0,mx=INT_MIN;
+    //     int m[26]={0};
+        
+    //     while(j<n){
+    //         m[S[j]-'a']++;
+    //         if(m[S[j]-'a']>1){
+    //             while(m[S[j]-'a']>1){
+    //                 m[S[i]-'a']--;
+    //                 i++;
+    //             }
+    //         }
+    //         mx=max(mx,j-i+1);
+    //         j++;
+    //     }
+    //     return mx;
     }
 };
 
