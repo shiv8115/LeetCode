dfd8f467925867de490f53799f8489967e1a7743
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-05-28 14:45:24 +0530

    Time: 212 ms (55.57%), Space: 83 MB (41.25%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n= nums.size();
-        for(int i=0;i<n;i++)
+        // int n= nums.size();
+        // for(int i=0;i<n;i++)
+        //     if(nums[i]<0 || nums[i]>n) nums[i]=0;
+        // int i=0;
+        // while(i<n){
+        //     if(nums[i]!=i+1 && nums[i]!=0 && nums[i]!=nums[nums[i]-1])
+        //         swap(nums[i],nums[nums[i]-1]);
+        //     else i++;
+        // }
+        // int ans=-1;
+        // for(int i=0;i<n;i++)
+        //     if(nums[i]!=i+1)
+        //     {
+        //         ans=i+1;
+        //         break;
+        //     }
+        // if(ans==-1) ans= n+1;
+        // return ans;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
             if(nums[i]<0 || nums[i]>n) nums[i]=0;
-        int i=0;
-        while(i<n){
-            if(nums[i]!=i+1 && nums[i]!=0 && nums[i]!=nums[nums[i]-1])
-                swap(nums[i],nums[nums[i]-1]);
-            else i++;
         }
-        int ans=-1;
+        for(int i=0;i<n;i++){
+            while(nums[i]>0 && nums[i]!= nums[nums[i]-1]) swap(nums[i], nums[nums[i]-1]);
+        }
         for(int i=0;i<n;i++)
-            if(nums[i]!=i+1)
-            {
-                ans=i+1;
-                break;
-            }
-        if(ans==-1) ans= n+1;
-        return ans;
+            if(nums[i]!=i+1) return i+1;
+    return n+1;
     }
 };
