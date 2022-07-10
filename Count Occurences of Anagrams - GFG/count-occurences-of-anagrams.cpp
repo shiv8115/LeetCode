07df23d4d605c79466b7ffc1dbbc400deb6bcf11
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-07-10 10:36:56 +0530

    Added solution - LeetHub

--- Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.cpp
+++ Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.cpp
@@ -10,31 +10,30 @@ class Solution{
 public:
 	int search(string pat, string txt) {
 	    // code here
-	    int n= txt.length();
-	    int k= pat.length();
-	    unordered_map<char,int>mp;
-	    int res=0;
-	    for(int i=0;i<k;i++) mp[pat[i]]++;
-	    int count=mp.size();
-	    int i=0,j=0;
-	    while(j<n){
-	        if(mp.find(txt[j])!=mp.end()){
-	            mp[txt[j]]--;
-	            if(mp[txt[j]]==0) count--;
-	        }
-	        if(j-i+1<k) j++;
-	        else if(j-i+1==k){
-	            // finding answer
-	            if(count==0) res++;
-	            if(mp.find(txt[i])!=mp.end()){
-	                mp[txt[i]]++;
-	                if(mp[txt[i]]==1) count++;
-	            }
-	            i++;
-	            j++;
-	        }
-	    }
-	    return res;
+	   int n=txt.length();
+	   int k= pat.length();
+	   unordered_map<char,int>mp;
+	   for(int i=0;i<k;i++) mp[pat[i]]++;
+	   int count=mp.size();
+	   int i=0,j=0;
+	   int res=0;
+	   while(j<n){
+	       if(mp.find(txt[j])!=mp.end()){
+	           mp[txt[j]]--;
+	           if(mp[txt[j]]==0) count--;
+	       }
+	       if(j-i+1<k) j++;
+	       else if(j-i+1==k){
+	           if(count==0) res++;
+	           if(mp.find(txt[i])!=mp.end()){
+	               mp[txt[i]]++;
+	               if(mp[txt[i]]==1) count++;
+	           }
+	           i++;
+	           j++;
+	       }
+	   }
+	   return res;
 	}
 
 };
