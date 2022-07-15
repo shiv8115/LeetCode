7e20604a33d09090662ee7275228ed1a72ca21c7
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-07-15 11:52:19 +0530

    Added solution - LeetHub

--- Unique BST's - GFG/unique-bsts.cpp
+++ Unique BST's - GFG/unique-bsts.cpp
@@ -4,27 +4,27 @@ using namespace std;
 
 
  // } Driver Code Ends
-
 class Solution
 {
     public:
     //Function to return the total number of possible unique BST. 
+    #define MOD 1000000007
     int numTrees(int n) 
     {
         // Your code here
-        long long  dp[n+1];
+        long long int dp[n+1];
         dp[0]=1;
         dp[1]=1;
         for(int i=2;i<=n;i++){
-            long long total = 0;
-            for(int j=1;j<=i;j++){
-                int left = j-1;
-                int right = i - j;
-                total+=(dp[left]*dp[right])%1000000007;
+            long long int cnt=0;
+            for(int j=0;j<i;j++){
+                long long int left=j;
+               long long  int right= i-j-1;
+                cnt+= (dp[left]* dp[right])%MOD;
             }
-            dp[i]=total%1000000007;
+            dp[i]= cnt%MOD;
         }
-        return (dp[n])%1000000007;
+        return dp[n]%MOD;
     }
 };
 
