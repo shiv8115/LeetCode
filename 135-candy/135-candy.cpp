45881becd5a5d6f286c8bb54ba6bbcfc4b1ea72a
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-07-19 18:55:42 +0530

    Time: 41 ms (34.79%), Space: 18.1 MB (34.53%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -25,7 +25,9 @@ public:
         vector<int>left(n,1),right(n,1);
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]) left[i]= left[i-1]+1;
-            if(ratings[n-1-i] > ratings[n-i]) right[n-1-i]= right[n-i]+1;
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]) right[i]= right[i+1]+1;
         }
         int ans=0;
         for(int i=0;i<n;i++){
