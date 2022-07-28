92b6cef95f6dbe3db1a749a451b81868425f2d25
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-07-29 01:28:23 +0530

    Time: 20 ms (72.33%), Space: 11.5 MB (71.56%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -1,41 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    int size(ListNode* head){
-        if(head == NULL){
-            return 0;
+    int getsize(ListNode* head){
+        int cnt=0;
+        while(head!=NULL){
+            cnt++;
+            head=head->next;
         }
-        
-        return 1 + size(head->next);
+        return cnt;
     }
-    ListNode* reverse(ListNode* head , int k , int size){
-        
-        if(head == NULL){
-           return head;
+    ListNode* solve(ListNode* head, int k, int n){
+        if(head==NULL) return NULL;
+        if(n<k) return head;
+        ListNode* curr= head, *next=head, *prev=NULL;
+        int cnt=0;
+        while(curr!=NULL && cnt<k){
+            next= curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            cnt++;
         }
-        if(size < k){
-            return head;
-        }
-        
-        int n = 0;
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        ListNode* next = head;
-        
-        while(next != NULL && n < k){
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-            n++;
-        }
-        
-        head->next = reverse(next , k , size - k);
-        
-        return prev; 
+        head->next= solve(next,k,n-k);
+        return prev;
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        int a = size(head);
-        ListNode* ans = reverse(head , k , a);
-        return ans;
+        int a= getsize(head);
+        ListNode* res= solve(head,k,a);
+        return res;
     }
 };
