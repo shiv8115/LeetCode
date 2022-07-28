afddc40a6ea682b53ac86456e217a0d63f685f89
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-07-29 01:23:03 +0530

    Time: 20 ms (72.33%), Space: 11.6 MB (29.90%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -1,39 +1,41 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head, int k){
-        ListNode* curr=head, *next=NULL, *prev=NULL;
-        while(curr && k){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-            k--;
+    int size(ListNode* head){
+        if(head == NULL){
+            return 0;
         }
-        return prev;
+        
+        return 1 + size(head->next);
+    }
+    ListNode* reverse(ListNode* head , int k , int size){
+        
+        if(head == NULL){
+           return head;
+        }
+        if(size < k){
+            return head;
+        }
+        
+        int n = 0;
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = head;
+        
+        while(next != NULL && n < k){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            n++;
+        }
+        
+        head->next = reverse(next , k , size - k);
+        
+        return prev; 
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        int count=0;
-        if(k==1) return head;
-        ListNode* curr=head, *next_head=NULL;
-        while(curr && count<k){
-            curr=curr->next;
-            count++;
-        }
-        if(count<k) return head;
-        next_head=curr;
-        ListNode* new_head= reverse(head,k);
-        head->next= reverseKGroup(next_head,k);
-        return new_head;
-        
+        int a = size(head);
+        ListNode* ans = reverse(head , k , a);
+        return ans;
     }
 };
