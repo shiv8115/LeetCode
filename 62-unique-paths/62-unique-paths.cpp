f54374391d7ccd571790f5c28b793ffd6f8e72a1
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-08-01 20:03:56 +0530

    Time: 3 ms (44.81%), Space: 6 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-       int dp[m+1][n+1];
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(i==1) dp[i][j]=1;
-                else if(j==1) dp[i][j]=1;
-                else{
-                    dp[i][j]= dp[i][j-1]+dp[i-1][j];
-                }
-            }
-        }
-       return dp[m][n];
+        // int dp[m][n];
+        // for(int i=m-1;i>=0;i--){
+        //     for(int j=n-1;j>=0;j--)
+        //     {
+        //         if(i==m-1) dp[i][j]=1;
+        //         else if(j==n-1) dp[i][j]=1;
+        //         else{
+        //             dp[i][j]= dp[i+1][j]+ dp[i][j+1];
+        //         }
+        //     }
+        // }
+        // return dp[0][0];
+        int N= m+n-2;
+        int r= m-1;
+        double res= 1.0;
+        for(int i=1;i<=r;i++) res= res*(N-r+i)/i;
+        
+        return (int)res;
+        
     }
 };
