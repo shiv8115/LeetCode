fc4582d4c548f6e0b344fa71021ecc7326a4184d
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-09-03 19:24:47 +0530

    Time: 76 ms (71.85%), Space: 26.1 MB (25.77%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    bool is_possible_sum(vector<int>&v, int mid, int days){
-        int d=1;
+    bool isOk(vector<int>&weights, int days, int mid){
+        int count=1;
         int sum=0;
-        for(int i=0;i<v.size();i++){
-            sum+=v[i];
+        for(int i=0;i<weights.size();i++){
+            sum+=weights[i];
             if(sum>mid){
-                d++;
-                sum=v[i];
+                sum=weights[i];
+                count++;
             }
         }
-        return d <= days;
+        return count<=days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
+        int low= *max_element(weights.begin(), weights.end());
+        int high= accumulate(weights.begin(), weights.end(),0);
         int ans=0;
-        int n=weights.size();
-        int maxm=0,sum=0;
-        for(auto val : weights){
-            sum+=val;
-            maxm=max(maxm,val);
-        }
-        if(days==n) return maxm;
-        int low=maxm, high=sum;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(is_possible_sum(weights,mid,days) == true){
-                ans= mid;
+        if(days==weights.size()) return low;
+        while(low<=high)
+        {
+            int mid= low+(high-low)/2;
+            if(isOk(weights,days,mid)){
+                ans=mid;
                 high=mid-1;
-            }else low=mid+1;
+            }
+            else low= mid+1;
         }
         return ans;
-        
     }
 };
