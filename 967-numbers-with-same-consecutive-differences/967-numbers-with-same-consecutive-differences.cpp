48a7401e8b96b301f9a49fcd86574e189c4832f0
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-09-03 23:37:59 +0530

    Time: 6 ms (42.98%), Space: 7.7 MB (70.71%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    vector<int> numsSameConsecDiff(int n, int k) {
-        if(n == 1) 
-            return {0,1,2,3,4,5,6,7,8,9};
-
-        queue<int> q;
-        for(int i=1;i<=9;i++)
-            q.push(i);
-        
-        while(--n){
-            int sz = q.size();
-            for(int i=0;i<sz;i++){
-                int cur = q.front();
-                q.pop();
-                
-                for(int j=0;j<10;j++){
-                    if(abs(cur%10 - j) == k)
-                        q.push(cur*10+j);
-                }   
+    vector<int>v;
+    int countf(int num){
+        int count=0;
+        while(num>0){
+            count++;
+            num= num/10;
+        }
+        return count;
+    }
+    void findfun(int num, int n, int k){
+        if(countf(num)==n){
+            v.push_back(num);
+            return;
+        }
+        for(int i=0;i<=9;i++){
+            int ld= num%10;
+            if(abs(ld-i)==k){
+                int number= (num*10)+i;
+                findfun(number,n,k);
             }
         }
-        
-        vector<int> res;
-        while(!q.empty())
-            res.push_back(q.front()), q.pop();
-        
-        return res;
+    }
+    vector<int> numsSameConsecDiff(int n, int k) {
+        for(int i=1;i<=9;i++){
+            findfun(i,n,k);
+        }
+        return v;
     }
 };
