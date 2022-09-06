cec1382c125ccb5583158d0e77c1e46246d50262
Shivam Kumar Singh <89718330+shiv8115@users.noreply.github.com>
2022-09-06 11:20:39 +0530

    Time: 175 ms (5.05%), Space: 22.1 MB (93.37%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -3,13 +3,9 @@ public:
     bool isOk(vector<int>&nums, int threshold, int mid){
         int sum=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]<=0){
-                sum+=1;
-            }
-            else{
                 sum+=(nums[i]/mid);
                 if(nums[i]%mid) sum+=1;
-            }
+       
         }
         return sum<=threshold;
     }
